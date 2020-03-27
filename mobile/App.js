f2b55eb4de92f9a399c341b682b964ad687e867c
alnmaurofranco <allanhps-singledcr@hotmail.com>
2020-03-27 06:47:35 -0300

    APP Mobile com React Native

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+import React from "react";
+
+import Routes from "./src/routes";
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
